08fe2c80352a24e454ac818a9e5b5d5429f0a29a
Jan Baudisch <dev@baudisch.xyz>
2017-10-02 17:53:17 +0200

    Update server syntax

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,10 @@
 extern crate ws;
 extern crate env_logger;
 
-use ws::listen;
-
 fn main () {
     env_logger::init().unwrap();
 
-    match listen("127.0.0.1:3333", |out| {
+    match ws::listen("127.0.0.1:3333", |out| {
         move |msg| {
             out.send(msg)
         }
